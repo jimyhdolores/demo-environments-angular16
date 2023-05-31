6c5cfdd2ebb120501456d392bca92ddce52e6dfb
jimmy <jimmyhd_19@hotmail.com>
2023-05-30 19:21:49 -0500

    son cosas pasan xd

--- src/environments/environment.common.ts
+++ src/environments/environment.common.ts
@@ -1,6 +1,7 @@
 export const commonEnvironment = {
-	api: import.meta.env.NG_APP_API,
-	valor1: 'valor 1',
-	valor2: 'valor 2',
-	valor3: 'valor 3'
+  api: import.meta.env.NG_APP_API,
+  valor1: 'valor 1',
+  valor2: 'valor 2',
+  valor3: 'valor 3',
+  valor4: 'valor 4',
 };
