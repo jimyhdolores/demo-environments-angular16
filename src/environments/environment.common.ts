15a2b04d7a0f96f6b6cf1d692e868614621a3407
jimmy <jimmyhd_19@hotmail.com>
2023-06-21 20:56:23 -0500

    probando ngx-env

--- src/environments/environment.common.ts
+++ src/environments/environment.common.ts
@@ -1,15 +1,7 @@
 export const commonEnvironment = {
-<<<<<<< HEAD
 	api: import.meta.env.NG_APP_API,
 	valor1: 'valor 1',
 	valor2: 'valor 2',
 	valor3: 'valor 3',
 	valor4: 'valor 4'
-=======
-  api: import.meta.env.NG_APP_API,
-  valor1: 'valor 1',
-  valor2: 'valor 2',
-  valor3: 'valor 3',
-  valor4: 'valor 4',
->>>>>>> 503d39344fa5533d104ce7d2ca42b10edf7e67a5
 };
